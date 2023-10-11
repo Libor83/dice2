149085f867e69235bb087288137c56963d11f7ca
Libor83 <126621465+Libor83@users.noreply.github.com>
2023-10-11 11:43:19 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,61 @@
+let náhoda = 0
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 10; index++) {
+        náhoda = randint(1, 6)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        if (náhoda == 1) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        }
+        if (náhoda == 2) {
+            basic.showLeds(`
+                . . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . .
+                `)
+        }
+        if (náhoda == 3) {
+            basic.showLeds(`
+                . . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . .
+                `)
+        }
+        if (náhoda == 4) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . #
+                `)
+        }
+        if (náhoda == 5) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . #
+                `)
+        }
+        if (náhoda == 6) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                # . . . #
+                . . . . .
+                # . . . #
+                `)
+        }
+    }
+})
